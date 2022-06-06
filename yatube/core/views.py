9e1e75ba71e6c20736f3251211735ec44f521250
Михаил Усенко <miha55rambler.ru@mail.ru>
2022-06-06 23:03:06 +0300

    Второе ревью 2.1

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -3,15 +3,25 @@ from http import HTTPStatus
 
 
 def page_not_found(request, exception):
-    return render(request, 'core/404.html', {'path': request.path}, status=HTTPStatus.NOT_FOUND)
+    return render(
+        request,
+        'core/404.html',
+        {'path': request.path},
+        status=HTTPStatus.NOT_FOUND)
 
 
 def server_error(request):
-    return render(request, 'core/500.html', status=HTTPStatus.INTERNAL_SERVER_ERROR)
+    return render(
+        request,
+        'core/500.html',
+        status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
 
 def permission_denied(request, exception):
-    return render(request, 'core/403.html', status=HTTPStatus.FORBIDDEN)
+    return render(
+        request,
+        'core/403.html',
+        status=HTTPStatus.FORBIDDEN)
 
 
 def csrf_failure(request, reason=''):
